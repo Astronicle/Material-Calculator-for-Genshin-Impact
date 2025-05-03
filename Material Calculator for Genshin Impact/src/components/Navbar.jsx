30c20564eeb3e1fe2d0cac3001e5f0cd7ec54d2d
Abdul Rehman <astronicle78@gmail.com>
2025-05-03 19:57:47 +0530

    Project Setup

---
+++ Material Calculator for Genshin Impact/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './components.css';
+
+function Navbar() {
+    return (
+        <>
+            <div className='navbar-container'>
+                <div>Material Calculator for Genshin Impact</div>
+            </div>
+
+        </>
+    )
+}
+
+export default Navbar;
