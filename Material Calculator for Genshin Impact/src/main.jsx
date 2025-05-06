44e240fe48faefa5b35efe2fb0323c3c7bbd6477
Abdul Rehman <astronicle78@gmail.com>
2025-05-06 18:52:34 +0530

    Added Vercel Analytics

--- Material Calculator for Genshin Impact/src/main.jsx
+++ Material Calculator for Genshin Impact/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
 import Calculator from './components/Calculator.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <Navbar/>
     <Calculator/>
   </StrictMode>,
